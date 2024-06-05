5132638883598d3ca169db97efb2aceb08d627f0
jdntntt <camcamanjadeantonette31@gmail.com>
2024-06-05 15:32:49 +0800

    Creating Patient Management

--- sanctum-api/routes/api.php
+++ sanctum-api/routes/api.php
@@ -1,19 +1,22 @@
 <?php
 
+use App\Http\Controllers\PatientController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+
+
+Route::get('patients', [PatientController::class, 'patients']);
+Route::get('showPatient/{id}', [PatientController::class, 'showPatient']);
+Route::post('registerPatients', [PatientController::class, 'registerPatients']);
+Route::put('editPatient/{id}', [PatientController::class, 'editPatient']);
+Route::delete('deletePatient/{id}', [PatientController::class, 'deletePatient']);
+Route::delete('removePatient/{id}', [PatientController::class, 'removePatient']);
