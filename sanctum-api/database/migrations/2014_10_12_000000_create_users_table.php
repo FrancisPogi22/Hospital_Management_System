e5980eca82f8b26943ec2e2dd7f368547e09a96d
John Dagohoy <dagohoyjohn80@gmail.com>
2024-06-06 11:51:55 +0800

    implementing Appointment Management

--- sanctum-api/database/migrations/2014_10_12_000000_create_users_table.php
+++ sanctum-api/database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,29 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('fullname');
+            $table->string('address')->nullable();
+            $table->string('contact')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->tinyInteger('account_type')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('users');
